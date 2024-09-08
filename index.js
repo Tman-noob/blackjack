18731dd98362426caa24fdfa856d100840a31c04
Tman-noob <91443186+Tman-noob@users.noreply.github.com>
2024-09-07 22:30:29 -0500

    Button added

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 let firstCard = 11
-let secondCard = 11
+let secondCard = 4
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
 
-if (sum <= 20) {
+function startGame() {
+    if (sum <= 20) {
     message = "Do you want to draw a new card?"
-} else if (sum === 21) {
-    message = "Woohoo! You've got Blackjack"
-    hasBlackJack = true
-} else {
-    message = "You're out of the game!"
-    isAlive = false
+    } else if (sum === 21) {
+        message = "Woohoo! You've got Blackjack"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    console.log(message)
 }
-
-console.log(message)
