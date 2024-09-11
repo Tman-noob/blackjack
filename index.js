8f98e9565a5e278c9aeb9df8414a51f5e6420936
Tman-noob <91443186+Tman-noob@users.noreply.github.com>
2024-09-10 19:59:17 -0500

    player added

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+let player = {
+    name: "Tim",
+    chips: 145
+}
 let cards = []
 let sum = 0
 let hasBlackJack = false
@@ -6,6 +10,9 @@ let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
+let playerEl = document.getElementById("player-el")
+
+playerEl.textContent = player.name + ": $" + player.chips
 
 function getRandomCard() {
     let randomNumber = Math.floor( Math.random() * 13) + 1
@@ -47,9 +54,11 @@ function renderGame() {
 }
 
 function newCard() {
-    let card = getRandomCard()
-    sum += card
-    cards.push(card)
-    console.log(cards)
-    renderGame()
+    if (isAlive === true && hasBlackJack === false) {
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        console.log(cards)
+        renderGame()
+    }
 }
